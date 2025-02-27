e566060ffa369938ff706c86ced92099e9136b2d
erick21340 <Torot0086@gmail.com>
2025-02-26 20:32:19 -0900

    agregado el ApiModule

---
+++ app/src/main/java/edu/ucne/erick_p2_p2/di/ApiModule.kt
@@ -0,0 +1,35 @@
+package edu.ucne.erick_p2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.erick_p2_p2.data.remote.api.DepositoManagerApi
+import javax.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    const val BASE_URL = "https://sagapi-dev.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesDepositoManagerApi(moshi: Moshi): DepositoManagerApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(DepositoManagerApi::class.java)
+    }
+}
