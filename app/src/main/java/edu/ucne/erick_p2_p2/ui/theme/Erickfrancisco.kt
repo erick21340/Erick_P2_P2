ad665e86378dffa25b0421ab17e504e20050a8e7
erick21340 <Torot0086@gmail.com>
2025-02-27 22:55:08 -0900

    Agregue  application

---
+++ app/src/main/java/edu/ucne/erick_p2_p2/ui/theme/Erickfrancisco.kt
@@ -0,0 +1,9 @@
+package edu.ucne.erick_p2_p2.ui.theme
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+    class Erickfrancisco : Application(){
+    }
