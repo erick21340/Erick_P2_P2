c87bc9dbbd6839d863959dfe9b71216d102a6dcc
erick21340 <Torot0086@gmail.com>
2025-02-27 22:51:06 -0900

    Actualize el DepositoManagerApi

--- app/src/main/java/edu/ucne/erick_p2_p2/data/remote/api/DepositoManagerApi.kt
+++ app/src/main/java/edu/ucne/erick_p2_p2/data/remote/api/DepositoManagerApi.kt
@@ -6,26 +6,32 @@ import okhttp3.ResponseBody;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Headers
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
-interface DepositoManagerApi {
+interface DepositoManagerApi{
+    @Headers("X-API-Key:test")
     @GET("api/Depositos")
-    suspend fun getDepositos(): List<DepositoDto>;
+    suspend fun getDepositos(): List<DepositoDto>
 
+    @Headers("X-API-Key:test")
     @GET("api/Depositos/{id}")
-    suspend fun getDeposito(@Path("id") id: Int): DepositoDto;
+    suspend fun getDeposito(@Path("id")id: Int): DepositoDto
 
+    @Headers("X-API-Key:test")
     @POST("api/Depositos")
-    suspend fun saveDeposito(@Body depositoDto: DepositoDto?): DepositoDto;
+    suspend fun saveDeposito(@Body depositoDto: DepositoDto?): DepositoDto
 
+    @Headers("X-API-Key:test")
     @PUT("api/Depositos/{id}")
     suspend fun actualizarDeposito(
-        @Path("id") depositoId: Int,
-        @Body deposito: DepositoDto
-    ): DepositoDto;
+        @Path("id")entityId: Int,
+        @Body entity: DepositoDto
+    ): DepositoDto
 
+    @Headers("X-API-Key:test")
     @DELETE("api/Depositos/{id}")
-    suspend fun deleteDeposito(@Path("id") id: Int): ResponseBody;
+    suspend fun deleteDeposito(@Path("id")id: Int): ResponseBody
 }
